86a7e6758e3b065cdfc5f7fdcb560ebac2302752
alirezaghodrati <apps.ghodrati1377@gmail.com>
2019-03-06 16:53:56 +0330

    Fix ShowSurveyUI To SDK Remove unnecesery Fix SDK Download Dialog

--- Assets/FiroozehGameServiceAndroid/Models/User.cs
+++ Assets/FiroozehGameServiceAndroid/Models/User.cs
@@ -6,17 +6,7 @@
 
         public string name { set; get; }
 
-        public string email { set; get; }
-
-        public string id { set; get; }
-
-        public string plan { set; get; }
-
         public string logo { set; get; }
-
-
-        public string mobile { set; get; }
-
 
         public int point { set; get; }
 
@@ -24,12 +14,7 @@
         {
             return "User{" +
                    "name='" + name + '\'' +
-                   ", email='" + email + '\'' +
-                   ", id='" + id + '\'' +
-                   ", plan='" + plan + '\'' +
-                   // ", created=" + created +
                    ", logo='" + logo + '\'' +
-                   ", mobile='" + mobile + '\'' +
                    ", point=" + point +
                    '}';
         }
