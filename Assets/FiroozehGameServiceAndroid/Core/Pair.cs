e1c8df2ed5da504f30606c8a497a54a3bb9a00ff
alirezaghodrati <apps.ghodrati1377@gmail.com>
2019-05-08 19:02:44 +0430

    Add CopyRight To Codes

--- Assets/FiroozehGameServiceAndroid/Core/Pair.cs
+++ Assets/FiroozehGameServiceAndroid/Core/Pair.cs
@@ -1,14 +1,36 @@
+// <copyright file="Pair.cs" company="Firoozeh Technology LTD">
+// Copyright (C) 2019 Firoozeh Technology LTD. All Rights Reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//    limitations under the License.
+// </copyright>
+
+/**
+* @author Alireza Ghodrati
+*/
+
+
+
 namespace FiroozehGameServiceAndroid.Core
 {
-    public class Pair<T,U> 
+    public class Pair<T,TU> 
     {
         
-        public Pair(T first, U second) {
+        public Pair(T first, TU second) {
             First = first;
             Second = second;
         }
 
         public T First { get; set; }
-        public U Second { get; set; }
+        public TU Second { get; set; }
     }
 }
--- Assets/FiroozehGameServiceAndroid/Models/Achievement.cs
+++ Assets/FiroozehGameServiceAndroid/Models/Achievement.cs
@@ -1,30 +1,50 @@
-﻿
+﻿// <copyright file="Achievement.cs" company="Firoozeh Technology LTD">
+// Copyright (C) 2019 Firoozeh Technology LTD. All Rights Reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//    limitations under the License.
+// </copyright>
+
 using System;
-
-[Serializable]
-public class Achievement
+/**
+* @author Alireza Ghodrati
+*/
+namespace FiroozehGameServiceAndroid.Models
 {
-
-    public string name { set; get; }
-    public string key { set; get; }
-    public string desc { set; get; }
-    public int point { set; get; }
-    public string image { set; get; }
-    public bool status { set; get; }
-    public string game { set; get; }
-    public bool earned { set; get; }
-    public override string ToString()
+    [Serializable]
+    public class Achievement
     {
-        return "Achievement{" +
-               "name='" + name + '\'' +
-               ", key='" + key + '\'' +
-               ", desc='" + desc + '\'' +
-               ", point=" + point +
-               ", image='" + image + '\'' +
-               ", status=" + status +
-               ", game='" + game + '\'' +
-               ", earned=" + earned +
-               '}';
+
+        public string name { set; get; }
+        public string key { set; get; }
+        public string desc { set; get; }
+        public int point { set; get; }
+        public string image { set; get; }
+        public bool status { set; get; }
+        public string game { set; get; }
+        public bool earned { set; get; }
+        public override string ToString()
+        {
+            return "Achievement{" +
+                   "name='" + name + '\'' +
+                   ", key='" + key + '\'' +
+                   ", desc='" + desc + '\'' +
+                   ", point=" + point +
+                   ", image='" + image + '\'' +
+                   ", status=" + status +
+                   ", game='" + game + '\'' +
+                   ", earned=" + earned +
+                   '}';
+        }
     }
 }
 
